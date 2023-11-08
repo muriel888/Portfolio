0426e1ad36a24e900533e12538523e498d1dcb52
muriel888 <tiendrebeogo.muriel@yahoo.com>
2023-11-07 22:49:01 -0500

    pokemon game

--- src/components/NavBar/Intro/intro.js
+++ src/components/NavBar/Intro/intro.js
@@ -19,4 +19,7 @@ const Intro = () => {
   );
 }
 
+
+
+
 export default Intro;
--- src/components/Projects/projects.js
+++ src/components/Projects/projects.js
@@ -12,9 +12,9 @@ const Projects = () => {
         <h3 className="projectsTitle">Mes Projects</h3>
         <div className="projectsImgs">
             <img src={ weather } alt="" className="projectsImg"/>
-            <a href='https://github.com/muriel888'><button className="workBtn">Voir Plus</button></a>
+            <a href='https://github.com/muriel888'><button className="workBtn">Voir plus</button></a>
             <img src={ cards } alt="" className="projectsImg"/>
-            <a href='https://github.com/muriel888'><button className="workBtn">Voir Plus</button></a>
+            <a href='https://muriel888.github.io/memory-game/'><button className="workBtn">Play the game</button></a>
             <img src={ game } alt="" className="projectsImg"/>
             <a href='https://github.com/muriel888'><button className="workBtn">Voir Plus</button></a>
             <img src={ cpro } alt="" className="projectsImg"/>
